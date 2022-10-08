99ee2ad24710c9bc76d92cb310999c86fab9a45e
CYNTHIA TOROITICH <cynthiajepkemei11@gmail.com>
2022-10-08 19:07:42 +0300

    First commit

---
+++ array.js
@@ -0,0 +1,14 @@
+var car = {
+    color : "blue";
+    speed : 300;
+    Made: "BMW";
+    Piston: [{maker: "BMW"}, {maker: "BMW"}]
+    clean: function() {return "clean";}
+};
+var Fruits = [
+    "orange",
+    "pinapple",
+    "Banana",
+    {juice: "passion"},
+    function() {return "drink";}
+];
